b594d00ef8fc6907732800a93152e031ac309d45
Rodrigo Marques <digoleeke@gmail.com>
2024-05-05 19:00:31 -0300

    feat:  env vars

--- web/index.php
+++ web/index.php
@@ -5,10 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
 defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['APP_DEBUG']);
 defined('YII_ENV') or define('YII_ENV', $_ENV['APP_ENV']);
+
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
